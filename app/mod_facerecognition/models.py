541fcb53b113ee0ed4d2e6ea29ae8284d32914e1
Kelvin Chot <kinyunye@gmail.com>
2019-04-29 06:40:09 +0300

    ++ Some files had not been added in last commit

---
+++ app/mod_facerecognition/models.py
@@ -0,0 +1,34 @@
+# Import the database object (db) from the main application module
+# We will define this inside /app/__init__.py in the next sections.
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, db.Sequence('user_id_seq'), primary_key=True, autoincrement=True)
+    username = db.Column(db.String(25), unique=True)
+    name = db.Column(db.String(150))
+    password = db.Column(db.String(191))
+    embeddings = db.Column(db.Text())
+
+    def __init__(self, username=None, password=None, name=None, embeddings=None):
+        self.username = username
+        self.name = name
+        self.embeddings = embeddings
+        self.password = password
+
+    def __repr__(self):
+        return "<User(id=%d, username='%s', name='%s', password='%s', embeddings='%s')>" % \
+               (self.id, self.username, self.name, self.password, self.embeddings)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'name': self.name,
+            'password': self.password,
+            'embeddings': self.embeddings
+        }
+
+
---
+++ run.py
@@ -0,0 +1,4 @@
+# Run a test server.
+# from app import app # face recognition app
+from app.demo import app # tutorial app
+app.run(host='0.0.0.0', port=8080, debug=True)
